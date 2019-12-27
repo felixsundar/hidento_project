6aa29df787409fe430f730b86f95c69105916f38
Felix Sundar <17445733+felixsundar@users.noreply.github.com>
2019-12-28 02:26:20 +0530

    user home complete

--- front_end/hidentojs/hidentoscript.js
+++ front_end/hidentojs/hidentoscript.js
@@ -1,1 +1,5 @@
-$(document).foundation();
+//var center_div_height = $("#page-center-div").height();
+//var center_margin_top = center_div_height/2;
+//var mtop_string = "-" + center_margin_top + "px";
+////alert(d);
+//$("#page-center-div").css("margin-top", mtop_string);
